909905e40d8fbb5fad6125d8e6c6d0978dac552f
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-10-17 00:01:38 +0530

    fix: join group button

--- components/Fossday/GameoverPage.tsx
+++ components/Fossday/GameoverPage.tsx
@@ -4,17 +4,16 @@ export default function GameoverPage() {
 	return (
 		<div className="w-screen h-screen overflow-hidden relative p-4 mt-7 flex items-start justify-center">
 			<div className="absolute bottom-0">
-					<img
-						src="./fossday/gameover.svg"
-						alt=""
-						className=" scale-150"
-					/>
-				</div>
+				<img src="./fossday/gameover.svg" alt="" className=" scale-150" />
+			</div>
 			<div className="text-4xl md:text-5xl text-white font-pixelify font-medium text-center p-4 md:w-[50vw] mt-9 flex flex-col items-center justify-center gap-9">
 				<p className="">
-				To contact us or to ask Any Questions , Join this group
+					To contact us or to ask Any Questions , Join this group
 				</p>
-				<button className="px-5 py-2 bg-white text-[#4fadf5] flex items-center justify-center rounded-full">Join Group</button>
+				{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+				<button className="px-5 py-2 bg-white text-[#4fadf5] flex items-center justify-center rounded-full">
+					Join Group
+				</button>
 			</div>
 		</div>
 	);
