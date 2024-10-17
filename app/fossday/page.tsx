dbd6106e346a36b02049578b9aee5d22ad946bbb
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-10-17 21:54:07 +0530

    feat: add call for speakers page

--- app/fossday/page.tsx
+++ app/fossday/page.tsx
@@ -4,6 +4,7 @@ import CommunityPage from "@/components/Fossday/CommunityPage";
 import GameoverPage from "@/components/Fossday/GameoverPage";
 import HomePage from "@/components/Fossday/HomePage";
 import RegistrationPage from "@/components/Fossday/RegistrationPage";
+import SpeakerPage from "@/components/Fossday/SpeakerPage";
 import TimelinePage from "@/components/Fossday/TimelinePage";
 import React from "react";
 
@@ -11,6 +12,7 @@ export default function FossDay() {
 	return (
 		<div className="h-auto w-screen bg-[#4fadf5] overflow-x-hidden">
 			<HomePage/>
+			<SpeakerPage/>
 			<AboutPage />
 			<TimelinePage/>
 			<CommunityPage/>
--- components/Fossday/SpeakerPage.tsx
+++ components/Fossday/SpeakerPage.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+import WavyText from "./WavyText";
 
 export default function SpeakerPage() {
-  return (
-    <div>
-        
-    </div>
-  )
+	const router = useRouter();
+
+	const handleRegisterClick = () => {
+		router.push("/fossday/speaker");
+	};
+	return (
+		<div className="relative flex h-screen w-screen flex-col items-center justify-around">
+			<div className="p-4 text-center">
+				<div className="relative mb-9 flex flex-col items-center justify-center md:mb-0 md:flex-row">
+					<img
+						src="./fossday/megaphone.png"
+						alt="megaphone"
+						className="mx-4 w-full md:w-[20rem]"
+					/>
+					<WavyText />
+				</div>
+				<div className="flex flex-col items-center justify-center gap-8">
+					<div className="w-full rounded-full border-2 border-yellow-500 border-dashed bg-white/30 px-6 py-6 font-pixelify text-3xl text-white backdrop-blur-md md:w-[70vw] md:px-6 md:py-4">
+						<p>
+							Submit your proposal today and join us in celebrating and
+							promoting FOSS!
+						</p>
+					</div>
+
+					{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+					{/* biome-ignore lint/nursery/useSortedClasses: <explanation> */}
+					<button
+						className="mb-6 w-[20rem] rounded-full bg-white px-6 py-2 font-pixelify text-2xl text-blue-500 shadow hover:shadow-lg"
+						onClick={handleRegisterClick}
+					>
+						Register Now
+					</button>
+				</div>
+			</div>
+
+			<img
+				src="./fossday/floor.png"
+				alt=""
+				className="h-20 w-auto md:h-auto md:w-full"
+			/>
+
+			{/* <img
+				src="./fossday/tree3.png"
+				alt=""
+				className="hidden md:block absolute bottom-0 left-40 transform -translate-x-1/2 -translate-y-2 w-[13%] scale-75"
+			/> */}
+		</div>
+	);
 }
