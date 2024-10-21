f0ad1badf4dd45a4df895da9ba3fcf8fdaff331c
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-10-21 22:51:00 +0530

    fix: image position in mobile view

--- components/Fossday/GameoverPage.tsx
+++ components/Fossday/GameoverPage.tsx
@@ -28,7 +28,7 @@ export default function GameoverPage() {
 					<img
 						src="./fossday/cloud5.png"
 						alt=""
-						className="w-60 -bottom-80 right-32 absolute animate-float delay-[0.9s]"
+						className="w-60 -bottom-80 -right-32 md:right-32 absolute animate-float delay-[0.9s]"
 					/>
 					<img
 						src="./fossday/cloud6.png"
