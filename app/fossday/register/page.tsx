cc7070c0fadcc6344cd9b50277a833ce373f8403
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-10-23 03:40:12 +0530

    feat: moved to makemypass ft. aswin ashok

--- app/fossday/register/page.tsx
+++ app/fossday/register/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
     return (
         <div className="relative h-screen overflow-auto">
             <iframe
-                title="Formbricks"
-                src="https://app.formbricks.com/s/cm2963qp50002nd57ci7rbu5u"
+                title="MakeMyPass"
+                src="https://makemypass.com/fossday"
                 frameBorder="0"
                 className="absolute top-0 left-0 h-full w-full border-0 bg-black"
             />
