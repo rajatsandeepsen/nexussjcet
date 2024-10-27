f916bb9f379fdf0eedb02104466ff9fa9462bef3
Milansuman <milansuman2016@gmail.com>
2024-10-27 16:03:22 +0530

    change workshops

--- components/Fossday/AboutPage.tsx
+++ components/Fossday/AboutPage.tsx
@@ -55,12 +55,16 @@ export default function AboutPage() {
 						<p className="px-2 text-lg rounded-full bg-green-200 text-green-500 font-bold w-fit h-fit">Beginner</p>
 					</div>
 					<div className="flex flex-col gap-2">
+						<Image src="/fossday/final-w2.png" alt="Intro to Github Actions" width={1080} height={1080}/>
+						<p className="px-2 text-lg rounded-full bg-blue-200 text-blue-500 font-bold w-fit h-fit">Novice</p>
+					</div>
+					<div className="flex flex-col gap-2">
 						<Image src="/fossday/final-w3.png" alt="Intro to Docker" width={1080} height={1080}/>
 						<p className="px-2 text-lg rounded-full bg-red-200 text-red-500 font-bold w-fit h-fit">Veteran</p>
 					</div>
 					<div className="flex flex-col gap-2">
-						<Image src="/fossday/final-w2.png" alt="Intro to Github Actions" width={1080} height={1080}/>
-						<p className="px-2 text-lg rounded-full bg-blue-200 text-blue-500 font-bold w-fit h-fit">Novice</p>
+						<Image src="/fossday/final-w4.png" alt="Intro to Github Actions" width={1080} height={1080}/>
+						<p className="px-2 text-lg rounded-full bg-red-200 text-red-500 font-bold w-fit h-fit">Veteran</p>
 					</div>
 				</div>
 			</div>
