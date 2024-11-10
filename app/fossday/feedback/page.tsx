9c15974862f558ab49699fa23a749b012bca0b02
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-11-10 14:08:25 +0530

    feat: FOSSFDAY feedback form

---
+++ app/fossday/feedback/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Page() {
+	return (
+		<div className="relative h-screen overflow-auto">
+			<iframe
+				title="Feedback Form"
+				src="https://app.formbricks.com/s/cm3bbtjzc0000fovlgvo0l94z"
+				className="absolute bottom-0 left-0 h-full w-full border-0"
+			/>
+		</div>
+	);
+}
