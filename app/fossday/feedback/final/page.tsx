6a674958998bd28eb416355fd70ab55a0d10e08d
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-11-17 14:57:01 +0530

    feat: final feedback form

---
+++ app/fossday/feedback/final/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Page() {
+	return (
+		<div className="relative h-screen overflow-auto">
+			<iframe
+				title="Final Feedback Form"
+				src="https://app.formbricks.com/s/arx8fzrs7wamnjtq8h7qiyr5"
+				className="absolute bottom-0 left-0 h-full w-full border-0"
+			/>
+		</div>
+	);
+}
