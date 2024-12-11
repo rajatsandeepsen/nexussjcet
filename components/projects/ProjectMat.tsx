9ad833360e553b05ea207f8fab3ba6d1cfa43224
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-12-11 10:20:05 +0530

    fix: minor responsive fixes

--- components/projects/ProjectMat.tsx
+++ components/projects/ProjectMat.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function ProjectMat() {
 	return (
 		
-		<div className="relative h-[35vh] w-full max-w-5xl rounded-lg drop-shadow-xl">
+		<div className="relative h-[35vh] w-[65vw]  rounded-lg drop-shadow-xl">
 			<Image
 				src={"/projects/tablemat.webp"}
 				width={1280}
 				height={720}
 				alt="logo"
-				className="absolute left-0 z-10  object-cover rounded-lg"
+				className="absolute md:top-[0%] top-1/2 -mt-14 z-10  object-cover rounded-lg"
 			/>
 			
 		</div>
--- components/projects/pages/RegisterPage.tsx
+++ components/projects/pages/RegisterPage.tsx
@@ -26,23 +26,23 @@ export default function RegisterPage() {
 		<div className="relative z-50 flex min-h-screen w-full flex-col items-center justify-between py-4 px-4 sm:px-6">
 			<Image
 				src="/projects/collegelogo.webp"
-				width={200}
-				height={200}
+				width={150}
+				height={150}
 				alt="College Logo"
-				className="mx-auto top-0  w-32 h-32 sm:w-48 sm:h-48 object-contain"
+				className="mx-auto top-0  w-32 h-32 sm:w-48 sm:h-48 object-contain absolute md:-top-10 top-0 "
 			/>
 			<Image
 				src="/projects/nexussticky.webp"
 				width={400}
 				height={400}
 				alt="Nexus Sticky"
-				className="-mt-12 sm:-mt-48 mx-auto w-32 h-32 sm:w-96 sm:h-96 object-contain md:top-1/2 top-1/4 absolute"
+				className="-mt-12 sm:-mt-48 mx-auto w-32 h-32 sm:w-96 sm:h-96 object-contain top-[35%] md:top-1/2 top-1/4 absolute"
 			/>
 			<div className="w-full mt-9 sm:mt-0 ">
 				<input
 					type="text"
 					placeholder="Enter your email"
-					className=" absolute left-[50%] md:left-1/2  top-[35%] md:top-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 rounded-full border-2 border-slate-300 md:px-4 md:py-2 text-base px-3 py-1 md:text-xl placeholder:text-sm sm:placeholder:text-base sm:text-lg"
+					className=" absolute left-[50%] md:left-1/2  top-[40%] md:top-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 rounded-full border-2 border-slate-300 md:px-4 md:py-2 text-base px-3 py-1 md:text-xl placeholder:text-sm sm:placeholder:text-base sm:text-lg"
 				/>
 			</div>
 			<div className="flex flex-wrap justify-center sm:justify-between items-center mt-8 sm:mt-0 w-full sm:w-3/4 gap-4">
